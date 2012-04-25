0435b34ea862f8349d664f733e0eb21e937cc7fd
Jervin R <accounts@dotmanila.com>
2012-04-25 08:03:46 -0400

    Added xbackup-run.sh wrapper, improved xbackup.sh

---
+++ xbackup-run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+### Local options
+WORK_DIR=/ssd/sb/xbackups
+CURDATE=$(date +%Y-%m-%d_%H_%M_%S)
+LOG_FILE="${WORK_DIR}/${CURDATE}.log"
+MAIL_FROM="root@`hostname`"
+MAIL_TO="me@example.com"
+MAIL_TO_ERROR="me@example.com"
+
+if [ -f $LOG_FILE ]; then rm -rf $LOG_FILE; fi
+
+/ssd/sb/scripts/xbackup.sh $1 ${CURDATE} 2>&1 | tee $LOG_FILE
+
+if [ "${PIPESTATUS[0]}" -ne 0 ]; then
+  (echo "Subject: ERROR: MySQL Backup failed on `hostname`";
+    cat ${WORK_DIR}/${CURDATE}-info.log;
+    if [ -f $LOG_FILE ]; then cat $LOG_FILE; fi;
+    ) | /usr/sbin/sendmail -O NoRecipientAction=add-to -f${MAIL_FROM} ${MAIL_TO_ERROR}
+else
+  (echo "Subject: MySQL Backup complete on `hostname`";
+    cat ${WORK_DIR}/${CURDATE}-info.log;
+  )  | /usr/sbin/sendmail -O NoRecipientAction=add-to -f${MAIL_FROM} ${MAIL_TO}
+fi
+
+
