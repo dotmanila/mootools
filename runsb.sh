7543b70cd4fb1f07bac98518dc0a9e1d7fbb191c
Jervin R <accounts@dotmanila.com>
2012-05-11 01:51:46 -0400

    Update to re-use prepared files per size.

--- runsb.sh
+++ runsb.sh
@@ -11,21 +11,33 @@ MOUNT=/ssd
 PART=/dev/sde1
 SB=/home/revin/bin/sysbench
 
-for tt in $TESTS
+for sz in $SIZES
 do
-for th in $THREADS
-do
-   for sz in $SIZES
+   umount $MOUNT
+   mkfs.xfs -fd su=64k,sw=2 $PART
+   mount $PART $MOUNT -o noatime,nobarrier,nodiratime
+   cd $MOUNT
+   $SB --test=fileio --file-num=64 --file-total-size=${sz}G prepare
+   sync
+
+   for th in $THREADS
    do
-      umount $MOUNT
-      mkfs.xfs -fd su=64k,sw=4 $PART
-      mount $PART $MOUNT -o noatime,nobarrier,nodiratime
-      cd $MOUNT
-      $SB --test=fileio --file-num=64 --file-total-size=${sz}G prepare
-      sync
-      echo 3 > /proc/sys/vm/drop_caches
-      $SB --test=fileio --file-total-size=${sz}G --file-test-mode=${tt} --max-time=${MAXTIME} --max-requests=0 --num-threads=${th} --rand-init=on --file-num=64 --file-extra-flags=direct --file-fsync-freq=0 --file-block-size=16384 --report-interval=10 run | tee $CWD/run-${tt}-${sz}G-${th}.txt
-      cd $CWD
+      for tt in $TESTS
+      do
+         $SB --test=fileio --file-total-size=${sz}G \ 
+            --file-test-mode=${tt} --max-time=${MAXTIME} \ 
+            --max-requests=0 --num-threads=${th} \ 
+            --rand-init=on --file-num=64 \ 
+            --file-extra-flags=direct --file-fsync-freq=0 \ 
+            --file-block-size=16384 --report-interval=10 run | tee $CWD/run-${tt}-${sz}G-${th}.txt
+         cd $CWD
+      # TESTS
+      done
+   # THREADS
    done
+# SIZES
 done
-done
+
+$SB --test=fileio --file-num=64 --file-extra-flags=direct --file-total-size=${sz}G cleanup
+
+
